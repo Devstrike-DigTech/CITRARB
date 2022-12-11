14ecdf21e79ec505d12af8ffce7fb74a7e1c3d8a
Richard Uzor <equality560@gmail.com>
2022-12-11 12:05:26 +0100

    added logo drawable files to project

---
+++ app/src/main/java/org/devstrike/app/citrarb/ui/features/SplashScreen.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022.
+ * Richard Uzor
+ * For Afro Connect Technologies
+ * Under the Authority of Devstrike Digital Ltd.
+ *
+ */
+
+package org.devstrike.app.citrarb.ui.features
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.devstrike.app.citrarb.R
+
+class SplashScreen : Fragment() {
+
+}
