5e9e521ed6c9afb9d276f8b520f52791fd9243d7
Richard Uzor <equality560@gmail.com>
2023-01-28 15:54:56 +0100

    modified some ui
    polished some ui up including lottie animations and navigation animations and designs

--- app/src/main/java/org/devstrike/app/citrarb/features/landing/ui/AppMenu.kt
+++ app/src/main/java/org/devstrike/app/citrarb/features/landing/ui/AppMenu.kt
@@ -13,9 +13,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.devstrike.app.citrarb.base.BaseFragment
 import org.devstrike.app.citrarb.features.landing.data.LandingMenu
 import org.devstrike.app.citrarb.databinding.FragmentAppMenuBinding
@@ -24,6 +28,10 @@ import org.devstrike.app.citrarb.features.landing.repositories.LandingRepo
 import org.devstrike.app.citrarb.features.news.newsLanding.data.remote.NewsListResponse
 import org.devstrike.app.citrarb.utils.Common
 import org.devstrike.app.citrarb.utils.Common.deepLinkNewsUrl
+import org.devstrike.app.citrarb.utils.SessionManager
+import org.devstrike.app.citrarb.utils.toast
+import javax.inject.Inject
+import kotlin.properties.Delegates
 
 /*
 * UI Fragment to display the app menu on the landing page
@@ -33,6 +41,9 @@ import org.devstrike.app.citrarb.utils.Common.deepLinkNewsUrl
 class AppMenu : BaseFragment<LandingViewModel, FragmentAppMenuBinding, LandingRepo>() {
 
     lateinit var toolBar: Toolbar
+
+    @set:Inject
+    var sessionManager: SessionManager by Delegates.notNull()
 
 
     private val landingMenuAdapter = LandingMenuAdapter()
@@ -52,21 +63,73 @@ class AppMenu : BaseFragment<LandingViewModel, FragmentAppMenuBinding, LandingRe
                 AppMenuDirections.actionAppMenuToNewsDetail(deepLinkNewsUrl, "", emptyNewsList)
             findNavController().navigate(navToNewsDetail)
         }
-        setUpRecyclerView()
+        //setUpRecyclerView()
+
+        setUpClickEvents()
 
     }
+
+    private fun setUpClickEvents() {
+        var token: String? = null
+        CoroutineScope(Dispatchers.IO).launch {
+            token = sessionManager.getJwtToken()
+        }
+        with(binding){
+            cardNews.setOnClickListener {
+                val navToNews = AppMenuDirections.actionAppMenuToNewsLanding()
+                findNavController().navigate(navToNews)
+            }
+            cardTv.setOnClickListener {
+                val navToTV = AppMenuDirections.actionAppMenuToTVVideoList()
+                findNavController().navigate(navToTV)
+            }
+
+            cardMembers.setOnClickListener {
+                val navToMembers = AppMenuDirections.actionAppMenuToMembersLanding()
+                if (token.isNullOrEmpty())
+                    requireContext().toast("Requires Login") //add an action to navigate to login screen
+                else
+                    findNavController().navigate(navToMembers)
+            }
+
+            cardEyeWitness.setOnClickListener {
+                //val navToEyeWitness = AppMenuDirections
+            }
+
+            cardMarketPlace.setOnClickListener {
+                //val navToMarketPlace = AppMenuDirections
+            }
+
+            cardEvents.setOnClickListener {
+                //val navToEvents = AppMenuDirections
+            }
+
+            cardConnect.setOnClickListener {
+                //val navToConnect = AppMenuDirections
+            }
+
+            cardMusic.setOnClickListener {
+                //val navToMusic = AppMenuDirections
+            }
+
+            cardUploads.setOnClickListener {
+                //val navToUploads = AppMenuDirections
+            }
+        }
+    }
+
 
     //function to set up views and logic for the recycler view UI
-    private fun setUpRecyclerView() {
-        binding.landingMenuGrid.apply {
-            adapter = landingMenuAdapter
-            layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
-
-        }
-        landingMenuAdapter.submitList(Common.landingMenuList)
-
-
-    }
+//    private fun setUpRecyclerView() {
+//        binding.landingMenuGrid.apply {
+//            adapter = landingMenuAdapter
+//            layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+//
+//        }
+//        landingMenuAdapter.submitList(Common.landingMenuList)
+//
+//
+//    }
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
@@ -76,6 +139,5 @@ class AppMenu : BaseFragment<LandingViewModel, FragmentAppMenuBinding, LandingRe
     override fun getFragmentRepo() = LandingRepo()
 
     override fun getViewModel() = LandingViewModel::class.java
-
 
 }
